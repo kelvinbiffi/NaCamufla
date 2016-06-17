840deb88fe86cc411f4404d415063bad51942be4
Kelvin <kelvin.biffi@objectedge.com>
2016-06-17 14:14:35 -0300

    Added resize function to calculate the height of chat-talk content

--- js/jQuery.js
+++ js/jQuery.js
@@ -4,5 +4,8 @@ $(document).ready(function(){
   console.log($("#chat").height());
   $("#chat-talk").height(($("#chat").height() - 200));
   console.log($("#chat-talk").height());
+  $( window ).resize(function(){
+    $("#chat-talk").height(($("#chat").height() - 200));
+  });
   
 });
