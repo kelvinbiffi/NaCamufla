cf0ec0a069efd76cf55db1a0cf1335cc6e45f278
Kelvin <kelvin.biffi@objectedge.com>
2016-07-15 09:32:50 -0300

    Added json file folder, added ftp service to upload archives, added ajax request to load json chat info

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,41 @@ var gulp = require('gulp-param')(require('gulp'), process.argv),
 	less = require('gulp-less'),
 	path = require('path'),
 	del = require('del'),
-	util = require('gulp-util'), 
+	util = require('gulp-util'),
 	ftp = require('gulp-ftp'),
 	jshint = require('gulp-jshint');
 
 //Routes for archives types
 var routes = {
+	scripts: ['js/*.js'],
 	less : ['css/*.less'],
+	images: 'images/*.*'
+};
+
+//FTP configuration
+var configFTP = function(path){
+	return {
+		host: 'xxx',
+		user: 'xxx',
+		pass: 'xxx',
+		remotePath: 'public_html/folder/' + path
+	};
 };
 
 //Clean CSS Files
 gulp.task('cleanCSS', function() {
-	return del(['css/main.css']);
+	return del(['css/*.css']);
 });
 
-//Concat LESS files
-gulp.task('conCSS',['cleanCSS'], function(){
-	return gulp.src(routes.less)
-	.pipe(concat('style.less'))
-	.pipe(gulp.dest('css'));
+//varify Syntax Code Errors
+gulp.task('JSCheck',function(){
+	return gulp.src(routes.scripts)
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'));
 });
 
 //Building LESS files in CSS files
-gulp.task('buildCSS', function() {
+gulp.task('buildCSS',['cleanCSS'], function() {
 	return gulp.src(routes.less)
 	.pipe(less({
 		paths: [ path.join(__dirname, 'less', 'includes') ]
@@ -33,8 +45,61 @@ gulp.task('buildCSS', function() {
 	.pipe(gulp.dest('css'));
 });
 
+//Publish Images Files
+gulp.task('pubChat', function(arch){
+	return gulp.src((arch === null ? 'chat/*' : 'chat/'+arch))
+	.pipe(ftp(configFTP('ws')))
+	.pipe(util.noop());
+});
+
+//Publish Images Files
+gulp.task('pubWS', function(arch){
+	return gulp.src((arch === null ? 'ws/*' : 'ws/'+arch))
+	.pipe(ftp(configFTP('ws')))
+	.pipe(util.noop());
+});
+
+gulp.task('deb', function(tt){
+	console.log(tt);
+});
+
+//Publish Images Files
+gulp.task('pubImg', function(arch){
+	return gulp.src((arch === null ? 'images/*' : 'images/'+arch))
+	.pipe(ftp(configFTP('images')))
+	.pipe(util.noop());
+});
+
+//Publish CSS Files
+gulp.task('pubCSS', function(){
+	return gulp.src('css/*.css')
+	.pipe(ftp(configFTP('css')))
+	.pipe(util.noop());
+});
+
+//Publish JS Files Had Build
+gulp.task('pubJS', function(){
+	return gulp.src('js/*.js')
+	.pipe(ftp(configFTP('js')))
+	.pipe(util.noop());
+});
+
+//Publish JS Dependencies Files
+gulp.task('pubHTML', function(){
+	return gulp.src('*.html')
+	.pipe(ftp(configFTP('')))
+	.pipe(util.noop());
+});
+
+//Publish All Source Files
+gulp.task('publish', ['pubImg','pubCSS','pubJS','pubDep','pubHTML'], function(){
+	return gulp.src('*')
+	.pipe(ftp(configFTP('')))
+	.pipe(util.noop());
+});
+
 //Default Gulp Action Watch buildJS and buildCSS
-gulp.task('default', ['buildCSS'], function(){
+gulp.task('default', ['buildJS','buildCSS'], function(){
 	gulp.watch(routes.scripts, ['buildJS']);
 	gulp.watch(routes.less, ['buildCSS']);
 });
--- js/Chatter.js
+++ js/Chatter.js
@@ -3,7 +3,9 @@ var app = angular.module("Chatter", [
     'ngSanitize' //Working with "ng-bind-html" to show html tags on interface
 ]);
 
-//Service to send info about user cross controlers
+/**
+ * Service to send info about user cross controlers
+ */
 app.service('userService', function() {
   var user = [];
   
@@ -19,7 +21,9 @@ app.service('userService', function() {
   };
 });
 
-//Service to send info about contacts cross controlers
+/**
+ * Service to send info about contacts cross controlers
+ */
 app.service('contactService', function() {
   var contactList = [];
 
@@ -35,24 +39,73 @@ app.service('contactService', function() {
   };
 });
 
-//Service to send info about chats cross controlers
-app.service('chatService', function() {
+/**
+ * Service to send info about chats cross controlers
+ */
+app.service('chatService', function(userService) {
   var chatTalk = [];
-
-  var setChat = function(newObj) {
-      chatTalk = newObj;
+  var chatTalkInfo = {};
+  
+  var scrollDown = function(time){
+    console.log('controlScroll');
+    var n = $('#chat-talk').height() * 100;
+    $('#chat-talk').animate({ scrollTop: n }, time);
   };
-  var getChat = function(){
-      return chatTalk;
+  
+  var setChatTalk = function(newObj) {
+    chatTalk = newObj;
   };
+  var getChatTalk = function(){
+    return chatTalk;
+  };
+  
+  var setChatInfo = function(newObj) {
+    chatTalkInfo = newObj;
+  };
+  var getChatInfo = function(){
+    return chatTalkInfo;
+  };
+  
+  var getChatTalk = function(){
+    
+  };
+  
+  var loadChatTalk = function(callback){
+    var user = userService.getUser();
+    console.log(user,'user');
+    var json = "http://kelvinbiffi.com/chatter/chat/" + chatTalkInfo.code + user[0].code + ".json";
+    console.log(json);
+    $.ajaxSetup({
+  	  cache:false
+  	});
+    $.ajax({
+      type: "GET",
+      dataType: 'json',
+      url: json,
+      data: {},
+      success: function (data) {
+        callback(data);
+      },
+      error: function(data, status, headers, config) {
+          console.log("error",data, status, headers, config);
+      }
+    });
+  };
+  
   return {
-    setChat: setChat,
-    getChat: getChat
+    setChatTalk: setChatTalk,
+    getChatTalk: getChatTalk,
+    setChatInfo: setChatInfo,
+    getChatInfo: getChatInfo,
+    scrollDown: scrollDown,
+    loadChatTalk: loadChatTalk
   };
 });
 
 
-//Service to manipulate text sent or read
+/**
+ * Service to manipulate text sent or read
+ */
 app.service('textService',function(){
   
   //***have to add video crawler***
@@ -110,13 +163,16 @@ app.service('textService',function(){
   };
 });
 
-//contacts API Controller
+/**
+ * contacts API Controller
+ */
 app.controller("contactsCtrl", function($scope, userService, contactService, chatService) {
   
   //Array user information must be loaded from web service
   $scope.user = [
     {code: "4g4gsgb", user : "kelvinbiffi", name : "Kelvin Biffi", info : "Only nature is secret"},
   ];
+  
   //Arrar avaliable contacts must be loaded from web service
   $scope.contacts = [
     {code: "8yg7", user : "leonelobiffi", name : "Leonelo Biffi", info : "Blue sky"},
@@ -125,59 +181,66 @@ app.controller("contactsCtrl", function($scope, userService, contactService, cha
     {code: "46j4y4j654", user : "kellenleote, angelastoll, fabiobueno, costinha234", name : "Zueira TI", info : "Zueira never ends"},
   ];
   
+  $scope.loadChat = function(contactInfo){
+    chatService.setChatInfo(contactInfo);
+  };
+  
   //send information to the services
   userService.setUser($scope.user);
   contactService.setContacts($scope.contacts);
 });
 
-//chat API controller
+
+/**
+ * chat API controller
+ */
 app.controller("chatCtrl", function($scope, userService, contactService, chatService, textService) {
   
   //Get information from services
   $scope.user = userService.getUser();
   $scope.contacts = contactService.getContacts();
   $scope.textService = textService;
-  $scope.oldScrollHeight = $('#chat-talk ul').height();
   
-  //must be loaded from contacts scope set above
-  $scope.chatInfo = [
-    {code: "46j4y4j654", user : "kellenleote, angelastoll, fabiobueno, costinha234", name : "Zueira TI", info : "Zueira never ends"},
-  ];
-  
-  //must be loaded from web service
-  $scope.chatTalk = [
-    {code: "8yg7", name : "Leonelo Biffi", text: "Hy", type: "contact"},
-    {code: "8yg7", name : "Leonelo Biffi", text: "How r u?", type: "contact"},
-    {code: "8yg7", name : "Leonelo Biffi", text: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?", type: "contact"},
-    {code: "4g4gsgb", name : "Kelvin Biffi", text: "Hey I'm fine and you man?", type: "user"},
-    {code: "4g4gsgb", name : "Kelvin Biffi", text: "Whata hell you doing now?", type: "user"},
-    {code: "8yg7", name : "Leonelo Biffi", text: "Im at the school, after I will go to the pub, do you want go?", type: "contact"},
-    {code: "8yg7", name : "Leonelo Biffi", text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)", type: "contact"},
-    {code: "4g4gsgb", name : "Kelvin Biffi", text: "Ohhhh I can't :/", type: "user"},
-    {code: "4g4gsgb", name : "Kelvin Biffi", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", type: "user"},
-    {code: "4g4gsgb", name : "Kelvin Biffi", text: "I will work until tonight, I will pass that, sorry", type: "user"},
-    {code: "8yg7", name : "Leonelo Biffi", text: "No problem man, I send you pictures kkk, have a good work", type: "contact"},
-    {code: "4g4gsgb", name : "Kelvin Biffi", text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.", type: "user"},
-    {code: "4g4gsgb", name : "Kelvin Biffi", text: "Thanks man!!", type: "user"},
-    {code: "8yg7", name : "Leonelo Biffi", text: "Thanks man!! https://media.giphy.com/media/xELN8T63yUJOM/giphy.gif", type: "contact"},
-    {code: "4g4gsgb", name : "Kelvin Biffi", text: "Noooooooooooo man!!", type: "user"},
-    
-  ];
-  
+  //Variables
+  $scope.chatInfo = [];
+  $scope.chatTalk = [];
   $scope.textArea = "";
   
+  //Watching chatInfo from contacts service set above
+  $scope.$watch(function () {
+    return chatService.getChatInfo();
+  }, function(currentChatInfo, oldChatInfo) {
+    $scope.chatInfo = [currentChatInfo];
+    
+    //if a contact was selected
+    if(!angular.equals({}, currentChatInfo)){
+      //must be loaded from web service and must be add spinner
+      chatService.loadChatTalk(function(data){
+        chatService.setChatTalk(data);
+        console.log($scope.chatTalk);
+        //Slide down chat content
+        var controlScrollDown = setTimeout(function(){
+          console.log('controlScrollTimeOut');
+          chatService.scrollDown(500);
+          clearTimeout(controlScrollDown);
+        },500);
+      });
+    }
+  }, true);
+  
+  $scope.$watch(function () {
+    return chatService.getChatTalk();
+  }, function(newChat, oldChat) {
+    $scope.chatTalk = newChat;
+  }, true);
+  
+  //Functions
   $scope.newMessage = function(){
     var self = this;
     self.code = $scope.user[0].code;
     self.name = $scope.user[0].name;
     self.text = $scope.textArea;
     self.type = "user";
-  };
-  
-  $scope.scrollDown = function(time){
-    console.log('controlScroll');
-    var n = $('#chat-talk').height() * 100;
-    $('#chat-talk').animate({ scrollTop: n }, time);
   };
   
   $scope.keyEnter = function(event){
@@ -192,20 +255,14 @@ app.controller("chatCtrl", function($scope, userService, contactService, chatSer
     if($scope.textArea.trim() !== ""){
       $scope.chatTalk.push(new $scope.newMessage());
       
-      $scope.scrollDown(500);
+      chatService.scrollDown(500);
       var controlScrollDown = setTimeout(function(){
         console.log('controlScrollTimeOut');
-        $scope.scrollDown(500);
+        chatService.scrollDown(500);
         clearTimeout(controlScrollDown);
       },500);
       $scope.textArea = "";
     }
   };
-  
-  var controlScrollDown = setTimeout(function(){
-    console.log('controlScrollTimeOut');
-    $scope.scrollDown(500);
-    clearTimeout(controlScrollDown);
-  },500);
   
 });
