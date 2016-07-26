65b14684f0eacd48eb1648545d339c0fdffdd0e0
kelvinbiffi <kelvin.biffi@objectedge.com>
2016-07-26 17:57:10 -0300

    added scroll at rooms content

--- js/jQuery.js
+++ js/jQuery.js
@@ -5,12 +5,14 @@ $(document).ready(function(){
   //When resize screen recalculate chat-talk height
   $("#chat-talk").height(($("#chat").height() - (width < 768 ? 150 : 200) ));
   $("#tuts").height(($("#chat").height() - (width < 768 ? 50 : 100) ));
+  $("#listRooms").height(($("#chat").height() - (width < 768 ? 100 : 100) ));
 
   //When resize screen recalculate chat-talk height
   $( window ).resize(function(){
     width = $(window).width()
     $("#chat-talk").height(($("#chat").height() - (width < 768 ? 150 : 200) ));
     $("#tuts").height(($("#chat").height() - (width < 768 ? 50 : 100) ));
+    $("#listRooms").height(($("#chat").height() - (width < 768 ? 100 : 100) ));
   });
 
 });
